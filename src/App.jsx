3c4caaeca420e9e7acdaa3f1c3b05dac2579fb65
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-16 16:29:24 +0300

    gateway

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { fetchCryptoList } from './gateway';
 
 const App = () => {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetchCryptoList().then(data => {
+      setCoins(data);
+    }).catch(error => new Error(console.log(error)));
+  }, []);
+
   return (
     <div>
       <h1 className="1">API</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/gateway.js
@@ -0,0 +1,10 @@
+const baseUrl =
+  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+
+export const fetchCryptoList = () => {
+  return fetch(baseUrl).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+  });
+};
