02a4981d56783571d2f4c6295ceaaddbe0a99f62
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-19 20:55:53 +0300

    setupTests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,3 @@
-// import { configure } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-// configure({ adapter: new Adapter() });
-
-import Enzyme from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
